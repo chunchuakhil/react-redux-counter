dbfacaabaa3d68f65a5f11811e7b24801f9f12ad
akhil chunchu <akhilchunchu52@gmail.com>
2020-11-07 17:20:50 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import countReducer from "./redux/stateReducer-stateChanger";
 
 const store = createStore(countReducer);
 //creates store with all reducrs/state changing functions
+//most of the business logic is present in the reducer, because reducers can change the state
 
 function App() {
   return (
